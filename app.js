e8c21be410a568900201d017b5f63f1a84e67fb9
AsrorjonTursunov <asrortursunov2009@gmail.com>
2026-09-24 10:28:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// function arrLenght(arr) {
+//   if (urn "bosh, ekran";
+//   } else {
+//     return arr.length;
+//   }
+// }
+// console.log(arrLenght(["1, 2, 3"]));
+// console.log(arrLenght([]))arr.length == 0) {
+//     ret
+
+/////============================================
+
+// // 31-chi misol
+// function calc(str) {
+//   return String(str);
+// }
+// console.log(calc(1, 2, 3, 4));
+
+/////============================================
+
+// 32 -chi misol
+
+// function teng10(x, y) {
+//   return x <= 10 && y <= 10;
+// }
+
+// console.log(teng10(11, 9));
+
+/////============================================
+
+// 33 -chi misol
+
+// function litrMasofa(km) {
+//   return km * 10;
+// }
+
+// console.log(litrMasofa(15   ));
+
+////============================================
+
+// 34 -chi misol
+
+// function fun(x, y) {
+//   if (x > y) {
+//     console.log(x);
+//   } else {
+//     console.log(y);
+//   }
+//   return x, y;
+// }
+// console.log(fun(9, 11));
+
+////============================================
+
+// 35 -chi misol
+
+// function arr(x, y) {
+//   return [x, y];
+// }
+// console.log(arr(5, 2));
+
+////============================================
+
+// 36 -chi misol
+// function tengStrings(str1, str2) {
+//   return str1.length == str2.length;
+// }
+// console.log(tengStrings("sa l", "asl"));
+
+////============================================
+
+// 37 -chi misol
+
+// function boshStr(str) {
+//   let b = 0;
+//   let a = str.length;
+//   return a == b;
+// }
+
+// console.log(boshStr(" "));
+
+function bolinsin5(son) {
+  return +son / 5 == 0;
+}
+
+console.log(bolinsin5(5));
